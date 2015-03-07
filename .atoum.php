3f358b5dd4226f796bed401e08fa909acaef047d
Tiross <Tiross@users.noreply.github.com>
2015-03-08 00:27:24 +0100

    Add coverage conf for local use

--- .atoum.php
+++ .atoum.php
@@ -7,7 +7,12 @@ use mageekguy\atoum\reports;
 $report = $script->addDefaultReport();
 $report->addField(new atoum\report\fields\runner\result\logo());
 
-$cloverWriter = new atoum\writers\file('./build/logs/clover.xml');
+$dir = './build/logs';
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$cloverWriter = new atoum\writers\file($dir . '/clover.xml');
 $cloverReport = new atoum\reports\asynchronous\clover();
 $cloverReport->addWriter($cloverWriter);
 
@@ -15,3 +20,13 @@ $runner->addReport($cloverReport);
 
 $testGenerator = new atoum\test\generator();
 $script->getRunner()->addTestsFromDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'tests');
+
+
+$dir = './coverage';
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+$coverage = new atoum\report\fields\runner\coverage\html('json', $dir);
+$coverage->setRootUrl('http://localhost');
+$report->addField($coverage);
